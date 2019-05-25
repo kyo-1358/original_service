2428451f84a4d63e94ffc97e78f7a3d34a2c9c18
kyo <r.kyo0821@icloud.com>
2019-05-25 09:15:43 +0000

    ログイン機能実装 #5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  root to: 'toppages#index'
   
   get 'signup', to: 'users#new'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :users, only: [:index, :show, :new, :create]
 end
