b6cda3851e6132f6ac90342a5bbcccdd3a07f528
kyo <r.kyo0821@icloud.com>
2019-06-04 05:44:33 +0000

    ランキング機能作成 #3

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,5 +1,6 @@
 class ToppagesController < ApplicationController
   def index
     @posts = Post.all
+    @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
   end
 end
