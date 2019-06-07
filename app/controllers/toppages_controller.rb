b979274088054076bb635bfbb3c95f87d86e2901
kyo <r.kyo0821@icloud.com>
2019-06-07 07:32:43 +0000

    新着トップ3作成 #9

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,5 +2,6 @@ class ToppagesController < ApplicationController
   def index
     @posts = Post.all
     @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
+    @sinchaku_posts = Post.order(created_at: :desc).limit(3)
   end
 end
